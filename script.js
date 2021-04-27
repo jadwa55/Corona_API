49e3a63cfb612a78a8aaf763ed5365ff4f9ed2eb
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2021-04-27 15:33:24 +0000

    AJAX TO JAVASCRIPT OBJET

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
+    //   document.getElementById("demo").innerHTML = this.responseText;
+    var  obj = JSON.parse(this.responseText);
+    // console.log(this.responseText);
+    // console.log(obj.data);
+    see(obj);
     }
+
   };
-  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.open("GET", "https://corona-api.com/countries/ma", true);
   xhttp.send();
 }
+
+function see(obj) {
+    
+}
+
+loadDoc();
