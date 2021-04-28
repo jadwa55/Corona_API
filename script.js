8afe9dd649afe63d734b86244b2f89b4ce484a26
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2021-04-28 11:11:10 +0000

    Static to dynamic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function loadDoc() {
+    var country = document.getElementById('country-code').value;
+    console.log(country);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -10,7 +12,8 @@ function loadDoc() {
     }
 
   };
-  xhttp.open("GET", "https://corona-api.com/countries/ma", true);
+  xhttp.open("GET","https://corona-api.com/countries/"+country, true);
+//   console.log(document.getElementById('country-code').value);
   xhttp.send();
 }
 
@@ -22,8 +25,9 @@ function see(obj) {
     document.querySelector("#deaths").innerText     = obj.data.latest_data.deaths;
     document.querySelector("#recovered").innerText  = obj.data.latest_data.recovered;
     document.querySelector("#population").innerText = obj.data.population;
-    document.querySelector("#time").innerText       = obj.data.timeline.date;
-    document.querySelector("#new-deaths").innerText = obj.data.timeline.new_deaths;
+    document.querySelector("#time").innerText       = obj.data.timeline[0].date;
+    // console.log(obj.data.timeline.date);
+    document.querySelector("#new-deaths").innerText = obj.data.timeline[0].new_deaths;
 
 
     document.getElementsByClassName("card")[0].style.display = "block";
